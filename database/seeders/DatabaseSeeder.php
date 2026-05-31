88c96b0704797c3b64f9897db3bb9ba5798d7959
Khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2026-05-31 21:02:25 +0100

    Create categories Tree

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Database\Seeders\CategorySeeder;
+
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -21,5 +23,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(CategorySeeder::class);
     }
 }
